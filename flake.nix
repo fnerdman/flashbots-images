884546538fc15cda5f6021efdd9d8cdc90eaa26f
alexhulbert <alex@alexhulbert.com>
2025-08-28 15:18:23 -0400

    Fix flake

--- flake.nix
+++ flake.nix
@@ -1,23 +1,27 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-  let
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
     system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
+    pkgs = import nixpkgs {inherit system;};
     reprepro = pkgs.stdenv.mkDerivation rec {
       name = "reprepro-${version}";
       version = "4.16.0";
 
       src = pkgs.fetchurl {
-        url = "https://alioth.debian.org/frs/download.php/file/"
-            + "4109/reprepro_${version}.orig.tar.gz";
+        url =
+          "https://alioth.debian.org/frs/download.php/file/"
+          + "4109/reprepro_${version}.orig.tar.gz";
         sha256 = "14gmk16k9n04xda4446ydfj8cr5pmzsmm4il8ysf69ivybiwmlpx";
       };
 
-      nativeBuildInputs = [ pkgs.makeWrapper ];
-      buildInputs = pkgs.lib.singleton (pkgs.gpgme.override { gnupg = pkgs.gnupg; })
-                ++ (with pkgs; [ db libarchive bzip2 xz zlib ]);
+      nativeBuildInputs = [pkgs.makeWrapper];
+      buildInputs =
+        pkgs.lib.singleton (pkgs.gpgme.override {gnupg = pkgs.gnupg;})
+        ++ (with pkgs; [db libarchive bzip2 xz zlib]);
 
       postInstall = ''
         wrapProgram "$out/bin/reprepro" --prefix PATH : "${pkgs.gnupg}/bin"
@@ -34,24 +38,39 @@
       };
       vendorHash = "sha256-NrZjORe/MjfbRDcuYVOGjNMCo1JGWvJDNVEPojI3L/g=";
     };
-    mkosi = system: 
-      let pkgsForSystem = import nixpkgs { inherit system; };
-      in pkgsForSystem.mkosi.override {
-        extraDeps = with pkgsForSystem; [
-          apt dpkg gnupg debootstrap
-          squashfsTools dosfstools e2fsprogs mtools mustache-go
-          cryptsetup util-linux zstd which qemu-utils parted
-        ] ++ [ reprepro ];
+    mkosi = system: let
+      pkgsForSystem = import nixpkgs {inherit system;};
+    in
+      pkgsForSystem.mkosi.override {
+        extraDeps = with pkgsForSystem;
+          [
+            apt
+            dpkg
+            gnupg
+            debootstrap
+            squashfsTools
+            dosfstools
+            e2fsprogs
+            mtools
+            mustache-go
+            cryptsetup
+            util-linux
+            zstd
+            which
+            qemu-utils
+            parted
+          ]
+          ++ [reprepro];
       };
   in {
     devShells = builtins.listToAttrs (map (system: {
       name = system;
-      value = pkgs.mkShell {
-        nativeBuildInputs = [ (mkosi system) measured-boot ];
+      value.default = pkgs.mkShell {
+        nativeBuildInputs = [(mkosi system) measured-boot];
         shellHook = ''
           mkdir -p mkosi.packages mkosi.cache mkosi.builddir ~/.cache/mkosi
         '';
       };
-    }) [ "x86_64-linux" "aarch64-linux" ]);
+    }) ["x86_64-linux" "aarch64-linux"]);
   };
 }
