b1be103a2d1a3699bb2989d2e6ee8f480cab5ffa
alexhulbert <alex@alexhulbert.com>
2025-09-02 17:37:13 -0400

    Autmatically use lima if it's installed but nix isn't

--- scripts/env_wrapper.sh
+++ scripts/env_wrapper.sh
@@ -6,7 +6,9 @@ LIMA_VM="${LIMA_VM:-tee-builder}"
 # Check if Lima should be used
 should_use_lima() {
     # Use Lima on macOS or if FORCE_LIMA is set
-    [[ "$OSTYPE" == "darwin"* ]] || [ -n "${FORCE_LIMA:-}" ]
+    [[ "$OSTYPE" == "darwin"* ]] || [ -n "${FORCE_LIMA:-}" ] || 
+    # Use Lima if it's available but Nix is not
+    (command -v limactl &>/dev/null && ! command -v nix &>/dev/null)
 }
 
 # Setup Lima if needed
